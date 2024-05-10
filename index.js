3b8073816aeadcd0fe27515708a53f6371d81990
Nabih <nabih.ahmad@hotmail.com>
2024-05-11 01:55:46 +0300

    Update env variable name

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const AWS = require('aws-sdk');
 
 // Configure the AWS SDK with your credentials and the Frankfurt region
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.DYNAMODB_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.DYNAMODB_AWS_SECRET_ACCESS_KEY,
     region: 'eu-central-1' // Frankfurt region
 });
 
